ea4a2a944db67beb659536eaa3ebf944313fdddf
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-11-20 13:09:27 -0300

    create aula 11

---
+++ curso-em-video/javascript/aula11/aula11.js
@@ -0,0 +1,13 @@
+var vel = 60
+console.log(`A velociadade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando sinto de segurança!')
+
+var pais = 'BR'
+if (pais != 'BR') {
+    console.log('Estrangeiro(a)')
+} else {
+    console.log('Brasileiro(a)')
+}
