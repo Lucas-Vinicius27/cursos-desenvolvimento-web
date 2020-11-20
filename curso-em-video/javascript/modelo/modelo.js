6f68d063570b45579acf7d1c714a2edcc482ec98
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-11-20 14:25:22 -0300

    create modelo

---
+++ curso-em-video/javascript/modelo/modelo.js
@@ -0,0 +1,7 @@
+
+// var hr = document.getElementById('horaAtual')
+
+// setInterval(function () {
+//     var date = new Date();
+//     hr.innerHTML = '<p><strong>' + date.toLocaleTimeString() + '</strong></p>';
+// }, 1000);
