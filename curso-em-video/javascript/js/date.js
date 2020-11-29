3ae9c958143406eb566e3b931ee5ab2e22727805
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-11-28 23:41:34 -0300

    update

--- curso-em-video/javascript/js/date.js
+++
@@ -1,6 +0,0 @@
-var hr = document.getElementById('horaAtual');
-
-setInterval(function () {
-    var date = new Date();
-    hr.innerHTML = '<p><strong>' + date.toLocaleTimeString() + '</strong></p>';
-}, 1000);
--- js/greeting.js
+++ js/greeting.js
@@ -1,13 +1,15 @@
 var hr = document.getElementById('horaAtual');
 var msg = document.querySelector('div#msg');
-var hora = new Date().getHours();
 
-if (hora >= 0 && hora < 12) {
-    msg.innerHTML = `<p><strong>Bom dia!</strong></p>`;
-} else if (hora >= 12 && hora <= 18) {
-    msg.innerHTML = `<p><strong>Boa tarde!</strong></p>`;
-} else {
-    msg.innerHTML = `<p><strong>Boa noite!</strong></p>`;
+if (msg != null) {
+    var hora = new Date().getHours();
+    if (hora >= 0 && hora < 12) {
+        msg.innerHTML = `<p><strong>Bom dia!</strong></p>`;
+    } else if (hora >= 12 && hora <= 18) {
+        msg.innerHTML = `<p><strong>Boa tarde!</strong></p>`;
+    } else {
+        msg.innerHTML = `<p><strong>Boa noite!</strong></p>`;
+    }
 }
 
 setInterval(function () {
