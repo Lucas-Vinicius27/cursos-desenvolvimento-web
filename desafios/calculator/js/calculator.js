5c801caef11fb23524f26fe861c1e7b448b34ce4
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-12-01 00:01:18 -0300

    create calculator.js

---
+++ desafios/calculator/js/calculator.js
@@ -0,0 +1,15 @@
+var n = [];
+var divConta = document.getElementById('conta');
+var divPreResultado = document.getElementById('pre-resultado');
+
+// function setN() {
+//     if (divConta.value == '') {
+//         divConta.innerHTML = 0;
+//     } else {
+//         divConta.innerHTML += 0;
+//     }
+// }
+
+// function backspace() {
+//     divConta.innerHTML = '';
+// }
