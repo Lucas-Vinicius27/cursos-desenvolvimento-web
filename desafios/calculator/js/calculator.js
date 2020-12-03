edff2757ecf10a3c158bb7ba1444319dcb3e87ff
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-12-03 19:09:29 -0300

    Update calculator.js

--- desafios/calculator/js/calculator.js
+++ desafios/calculator/js/calculator.js
@@ -1,22 +1,30 @@
 var stringConta = '';
+var stringUI = '';
 var inputConta = document.getElementById('conta');
 var inputResultado = document.getElementById('resultado');
 
 function setConta(str) {
     if (stringConta == '' || stringConta == '0') {
         stringConta = str;
-        inputConta.setAttribute('value', stringConta);
+        stringUI = str;
+        inputConta.setAttribute('value', stringUI);
     } else if (str == '.') {
         stringConta += str;
-        inputConta.setAttribute('value', ',');
+        stringUI = stringConta.replace('.', ',');;
+        inputConta.setAttribute('value', stringUI);
+    } else if (str == '*') {
+        stringConta += str;
+        stringUI = stringConta.replace('*', 'x');
+        inputConta.setAttribute('value', stringUI);
     } else {
         stringConta += str;
-        inputConta.setAttribute('value', stringConta);
+        stringUI += str;
+        inputConta.setAttribute('value', stringUI);
     }
 }
 
 function resultado() {
-    if (stringConta == '') {
+    if (stringConta == '' || stringUI == '') {
         alert('Não há conta para exibir o resultado!');
     } else {
         inputResultado.setAttribute('value', eval(stringConta));
@@ -24,7 +32,11 @@ function resultado() {
 }
 
 function backspace() {
-    inputConta.setAttribute('value', '');
+    let ui = stringUI.slice(0, stringUI.length - 1);
+    stringUI = ui;
+    let conta = stringConta.slice(0, stringConta.length - 1);
+    stringConta = conta;
+    inputConta.setAttribute('value', stringUI);
 }
 
 function limpar() {
