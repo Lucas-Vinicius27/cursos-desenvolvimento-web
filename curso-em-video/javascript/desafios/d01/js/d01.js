3de62879607fe90ed440af7eda631e6d1ed3904a
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-12-18 18:36:08 -0300

    desafio 01

---
+++ curso-em-video/javascript/desafios/d01/js/d01.js
@@ -0,0 +1,17 @@
+function msg() {
+    alert('oi');
+    console.log(n);
+    switch(n) {
+        case 1:
+            alert(`Você clicou no ${n}º botão!`);
+            break;
+        case 2:
+            alert(`Você clicou no ${n}º botão!`);
+            break;
+        case 3:
+            alert(`Você clicou no ${n}º botão!`);
+            break;
+        default:
+            alert('default');
+    }
+}
