f2993b6eba86be58f6401c8a5dab4b8dc6267bc9
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-12-20 17:15:27 -0300

    Desafio 01 terminado

--- curso-em-video/javascript/desafios/d01/js/d01.js
+++ curso-em-video/javascript/desafios/d01/js/d01.js
@@ -1,17 +1,3 @@
-function msg() {
-    alert('oi');
-    console.log(n);
-    switch(n) {
-        case 1:
-            alert(`Você clicou no ${n}º botão!`);
-            break;
-        case 2:
-            alert(`Você clicou no ${n}º botão!`);
-            break;
-        case 3:
-            alert(`Você clicou no ${n}º botão!`);
-            break;
-        default:
-            alert('default');
-    }
+function mensagem(n) {
+    alert(`Você clicou no ${n}º botão!`);
 }
