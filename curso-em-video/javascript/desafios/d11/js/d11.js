c89b48a68c460f9b0602534f5190155f272383be
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-12-21 22:51:11 -0300

    Desafio 11

---
+++ curso-em-video/javascript/desafios/d11/js/d11.js
@@ -0,0 +1,6 @@
+function verificar() {
+    let res = document.getElementById('res');
+    let ano = prompt('Qual é o ano que você quer verificar?');
+    let diasAno = Date();
+    alert(diasAno)
+}
