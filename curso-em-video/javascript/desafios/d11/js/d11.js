158b4f1f83e54ac8c374897fefd5042f7b1ecd6c
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2020-12-30 13:53:08 -0300

    Update d11.js

--- curso-em-video/javascript/desafios/d11/js/d11.js
+++ curso-em-video/javascript/desafios/d11/js/d11.js
@@ -1,6 +1,13 @@
 function verificar() {
     let res = document.getElementById('res');
     let ano = prompt('Qual é o ano que você quer verificar?');
-    let diasAno = Date();
-    alert(diasAno)
+    let dia = new Date(ano, 1, 29).getDate();
+
+    if (dia == 29) {
+        res.innerHTML = `<h2>Analisando o ano de ${ano}...</h2>`
+            + `<p>O ano de ${ano} <mark style="background: #7CFC00"><strong>É BISSEXTO</strong></mark> \u{2714}\u{FE0F}</p>`;
+    } else {
+        res.innerHTML = `<h2>Analisando o ano de ${ano}...</h2>`
+            + `<p>O ano de ${ano} <mark style="background: #FF4040"><strong>NÃO É BISSEXTO</strong></mark> \u{274C}</p>`;
+    }
 }
