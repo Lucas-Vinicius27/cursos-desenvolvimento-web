3b64df27573346a25c920a6890ba97f629f8e919
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-01-10 15:27:03 -0300

    Update game.js

--- desafios/game-one/js/game.js
+++ desafios/game-one/js/game.js
@@ -1,20 +1,23 @@
 var nome = prompt('Qual o seu nome?').trim();
 var divWelcome = document.getElementById('welcome');
 var divQuestion = document.getElementById('question');
-var imgDown = document.createElement('img');
 
 divWelcome.innerHTML = `<h2>Seja Bem-Vindo ${nome}!</h2>`
     + '<p>Aqui estão as perguntas sobre animes:</p>';
 
-imgDown.setAttribute('class', 'card');
-imgDown.setAttribute('src', 'images/face-down.png');
-imgDown.setAttribute('alt', 'Fundo da Carta');
-imgDown.setAttribute('onclick', "teste()");
-
-for (let i = 0; i <= 10; i++) {
+for (let i = 0; i <= 9; i++) {
+    var imgDown = document.createElement('img');
+    imgDown.setAttribute('class', 'card');
+    imgDown.setAttribute('src', 'images/face-down.png');
+    imgDown.setAttribute('alt', 'Fundo da Carta');
+    imgDown.setAttribute('onclick', `openModal(${i})`);
     divQuestion.appendChild(imgDown);
 }
 
-function teste() {
-    alert('olá')
+function openModal(n) {
+    // let divModal = document.createElement('div').setAttribute('id', `card${n}`);
+    // divModal.style.display = 'block';
+    // divModal.innerHTML = `<span onclick="divModal.style.display = 'none'">&times;</span>`
+    //     + `<p>${nome} você conseguiu fazer um modal com a carta ${n}</p>`;
+    // document.innerHTML = divModal;
 }
