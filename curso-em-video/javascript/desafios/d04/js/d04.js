8104a1e57da0d9401afdebb2f2cd4e22b52e01d6
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-02-21 15:17:32 -0300

    Update d04.js

--- curso-em-video/javascript/desafios/d04/js/d04.js
+++ curso-em-video/javascript/desafios/d04/js/d04.js
@@ -1,6 +1,6 @@
 function calcular() {
     let nome = prompt('Qual produto você está comprando?');
-    let preco = Number(prompt(`Qunato custa ${nome} que você está comprando?`));
+    let preco = Number(prompt(`Quanto custa ${nome} que você está comprando?`));
     let pago = Number(prompt(`Qual o valor que você deu para pagar ${nome}?`));
     let troco = pago - preco;
     alert(`Você comprou ${nome} que custou ${preco.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}.
